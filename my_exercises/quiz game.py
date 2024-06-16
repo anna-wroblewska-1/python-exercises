7a65eaff50d6857dfc231f9f7d226e3bc6964151
Anna Wróblewska <annawrooblewska@gmail.com>
2024-06-16 19:32:52 +0200

    Added for loop

--- my_exercises/quiz game.py
+++ my_exercises/quiz game.py
@@ -10,42 +10,19 @@ quiz = {"How many legs have a spider?": "8",
         "What color is an elephant?": "grey",
         "Who is bigger: alligator or crocodile?": "crocodile"
         }
-q1 = list(quiz.keys())[0]
-q2 = list(quiz.keys())[1]
-q3 = list(quiz.keys())[2]
 
 print("Hi! This is Animal Quiz.\nLet's get started!\n")
 
 score = 0
 
-print(q1)
-answer = str(input("Answer: "))
-if answer in list(quiz.values())[0]:
-    print("Great!")
-    score += 1
-    print(f"Your score: {score}")
-else:
-    print("Wrong! Try next time")
-    print(f"Your score: {score}")
-
-print(q2)
-answer = str(input("Answer: ").lower())
-if answer in list(quiz.values())[1]:
-    print("Great!")
-    score += 1
-    print(f"Your score: {score}")
-else:
-    print("Wrong! Try next time")
-    print(f"Your score: {score}")
-
-print(q3)
-answer = str(input("Answer: ").lower())
-if answer in list(quiz.values())[2]:
-    print("Great!")
-    score += 1
-    print(f"Your score: {score}")
-else:
-    print("Wrong! Try next time")
+for key, value in quiz.items():
+    print(key)
+    answer = str(input("Answer: ").strip().lower())
+    if answer == value:
+        print("Great!")
+        score += 1
+    else:
+        print("Wrong! Try next time")
     print(f"Your score: {score}")
 
 print("That's it. I hope you had fun!")
